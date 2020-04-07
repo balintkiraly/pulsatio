33657a964456bd1c477e5a61150b9575c63679e5
balintkiraly <balint.kiraly@protonmail.com>
2020-04-07 20:53:30 +0200

    Add chakraUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <CSSReset />
+      <img src={logo} className="App-logo" alt="logo" />
+    </ThemeProvider>
   );
-}
-
-export default App;
+};
